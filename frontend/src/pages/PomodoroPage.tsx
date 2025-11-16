e3a43fb5106dd640d76258da4341d57b1fa1052c
Mikolaj Jedrzejczak <mik.jed2003@gmail.com>
2025-11-16 19:11:46 +0100

    feat(frontend): add pomodoro page skeleton and route

---
+++ frontend/src/pages/PomodoroPage.tsx
@@ -0,0 +1,14 @@
+import { Navbar } from '../layout/Navbar';
+
+const PomodoroPage = () => {
+  return (
+    <div>
+      <Navbar />
+      <main style={{ padding: '2rem' }}>
+        <h1>Strona Pomodoro</h1>
+      </main>
+    </div>
+  );
+};
+
+export default PomodoroPage;
